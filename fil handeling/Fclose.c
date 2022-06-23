650d7571002529e6f93fc7adcde7c21d833e81bc
wahid1099 <wahidahmed890@gmail.com>
2022-06-23 15:37:06 +0600

    adding

--- fil handeling/Fclose.c
+++ fil handeling/Fclose.c
@@ -3,8 +3,20 @@
 int main()
 {
  
+ FILE *inputFIle=fopen("input.txt", "r");
+ FILE *outputFIle=fopen("output2.txt", "w");
+ if(inputFIle==NULL){
+    printf("Error");
+    return 0;
+ }
+
+ int n;
+
+ fscanf(inputFIle, "%d", &n);
+ fprintf(outputFIle, "%d", n);
 
  
- 
+ fclose(inputFIle);
+ fclose(outputFIle);
   return 0;
 }
