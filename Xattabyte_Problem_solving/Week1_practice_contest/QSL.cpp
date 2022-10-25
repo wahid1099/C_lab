e8a1bf5a9e73782af6b11c83338ed2744d87b8ba
wahid1099 <wahidahmed890@gmail.com>
2022-10-25 16:30:52 +0600

    adding new

--- Xattabyte_Problem_solving/Week1_practice_contest/QSL.cpp
+++ Xattabyte_Problem_solving/Week1_practice_contest/QSL.cpp
@@ -5,15 +5,21 @@ using namespace std;
 int main()
 {
  
- long long int n;
+ long long int n,result;
 
  while(cin >> n){
     if(n == 0) break;
+
+
     if(n>100){
-        cout<<"f91("<<n<<") "<<"= "<<n-10<<endl;
-    }else if(n>=100){
-         cout<<"f91("<<n<<") "<<" = "<<n<<endl;
+        result=n-10;
+       
+    }else if(n<=100){
+        result=91;
+         
     }
+
+     cout<<"f91("<<n<<") "<<"= "<<result<<endl;
  }
  
  
